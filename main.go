f2060e750f5a859e5f0b52a431fe684af8d2cf4c
Faizin Ahsan <mochamad.faizin@pactindo.com>
2026-01-12 17:32:58 +0700

    fix: connection env and dockerfile

--- main.go
+++ main.go
@@ -43,7 +43,8 @@ func main() {
 
 	app.Get("/hello", handler.HelloWorld)
 	transaction := app.Group("/transactions")
-	transaction.Get("/download", handler.GetTransactions)
+	transaction.Get("/generate-file", handler.GetTransactions)
+	transaction.Get("/download", handler.DownloadTransactionFile)
 
 	port := viper.GetString("server.port")
 	if port == "" {
@@ -72,7 +73,7 @@ func (h *Handler) GetTransactions(c *fiber.Ctx) error {
 	start := time.Now()
 	var mStart, mEnd runtime.MemStats
 	runtime.ReadMemStats(&mStart)
-	log.Info("Received request to download transactions")
+	log.Info("Received request to generate transactions")
 
 	totalData := 0
 	if totalDataReq := c.Query("total_data"); totalDataReq != "" {
@@ -99,7 +100,24 @@ func (h *Handler) GetTransactions(c *fiber.Ctx) error {
 	elapsed := time.Since(start)
 	log.Infof("Memory Usage: Alloc = %.2f MB, TotalAlloc = %.2f MB, Sys = %.2f MB, NumGC = %v", float64(mEnd.Alloc-mStart.Alloc)/1024/1024, float64(mEnd.TotalAlloc-mStart.TotalAlloc)/1024/1024, float64(mEnd.Sys-mStart.Sys)/1024/1024, mEnd.NumGC-mStart.NumGC)
 	defer func() {
-		err = os.Remove("./" + filename)
+		log.Infof("Execution Time: %v", elapsed)
+	}()
+	return c.Status(200).JSON(fiber.Map{
+		"message": "File generated successfully: " + filename,
+	})
+}
+
+func (h *Handler) DownloadTransactionFile(c *fiber.Ctx) error {
+	start := time.Now()
+	var mStart, mEnd runtime.MemStats
+	runtime.ReadMemStats(&mStart)
+	log.Info("Received request to download transactions")
+	filename := c.Query("filename")
+	defer func() {
+		runtime.ReadMemStats(&mEnd)
+		elapsed := time.Since(start)
+		log.Infof("Memory Usage: Alloc = %.2f MB, TotalAlloc = %.2f MB, Sys = %.2f MB, NumGC = %v", float64(mEnd.Alloc-mStart.Alloc)/1024/1024, float64(mEnd.TotalAlloc-mStart.TotalAlloc)/1024/1024, float64(mEnd.Sys-mStart.Sys)/1024/1024, mEnd.NumGC-mStart.NumGC)
+		err := os.Remove("./" + filename)
 		if err != nil {
 			log.Errorf("Failed to remove file %s: %v", filename, err)
 		} else {
